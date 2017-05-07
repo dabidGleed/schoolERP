f7636343e49a4e946740d49eb3684ccea7f227e1
chmahi <maheswari.chnaidu@gmail.com>
2017-05-07 06:27:31 -0700

    added controller nd service in exam schedule

--- app/components/examSchedules/examServices.js
+++ app/components/examSchedules/examServices.js
@@ -18,6 +18,21 @@
                     headers: { 'Content-Type': 'application/x-www-form-urlencoded'},
                 })
       };
+        examServices.getExamPapers = function(){
+        return $http({
+                    method: 'GET',
+                    url: globalServices.globalValue.baseURL + 'api/exams/SCH-9271-CL-11-SUB-3/SCH-9271-EXM_SCH-39'
+                })
+    };
+     examServices.setExamPapers = function(dataValue){
+         console.log(dataValue);
+        return $http({
+                    method: 'POST',
+                    url: globalServices.globalValue.baseURL + 'api/exams/SCH-9271-CL-11-SUB-3/SCH-9271-EXM_SCH-39',
+                    data: $.param(dataValue),
+                    headers: { 'Content-Type': 'application/x-www-form-urlencoded'},
+                })
+      };
 
       return examServices;
 
