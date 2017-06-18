a370943f816bb0d6df688adff71f8f7133b0fb92
chmahi <maheswari.chnaidu@gmail.com>
2017-06-19 00:29:46 +0530

    added some fixes

--- app/components/addBook/addBookController.js
+++ app/components/addBook/addBookController.js
@@ -1,6 +1,45 @@
 angular.module('school_erp')
-.controller("addBookController",['$http','$scope','addBookServices','ngDialog', function($http, $scope, addBookServices, ngDialog){
+.controller("addBookController",['$http','$scope','addBookServices','ngDialog','globalServices','subjectsServices', function($http, $scope, addBookServices, ngDialog, globalServices, subjectsServices){
         $scope.data = [];
+         $scope.today1 = '01/01/2000';
+
+        globalServices.getClass()
+        .success(function(data, status){
+            $scope.classData = data.school_classes;// Api list-name
+            $scope.classId = $scope.classData[0].class_id;
+            $scope.populateSections($scope.classId)
+            
+        })
+        .error(function(data,success){
+        })
+
+         $scope.populateSections = function(classId){
+             $scope.secId =[]; 
+            globalServices.getSections(classId)
+            .success(function(data, status){
+                $scope.secData = data.class_sections;// Api list-name
+                $scope.secId = $scope.secData[0].section_id;
+                $scope.populateSubjects($scope.secId);
+                
+            })
+            .error(function(data,success){
+                
+                $scope.populateSubjects($scope.secId);
+            })
+        } 
+
+          $scope.populateSubjects = function(secId){
+            $scope.subData = [];
+            subjectsServices.getSubjects(secId)
+            .success(function(data, status){
+                $scope.subData = data.subjects;
+                $scope.subId = $scope.subData[0].subject_id;
+                $scope.getChapters($scope.subId);
+            })
+            .error(function(data,success){
+            });
+        }
+
          $scope.addBook= function(data){
              var bookDetails = {
                     book_title:$scope.data.book_title,
--- app/components/classWise/classWiseController.js
+++ app/components/classWise/classWiseController.js
@@ -1,5 +1,42 @@
 angular.module('school_erp')
-.controller("classWiseController",['$http','$scope','globalServices', function($http, $scope,globalServices){
+.controller("classWiseController",['$http','$scope','globalServices','subjectsServices', function($http, $scope,globalServices, subjectsServices){
+         $scope.data = [];
+         globalServices.getClass()
+        .success(function(data, status){
+            $scope.classData = data.school_classes;// Api list-name
+            $scope.classId = $scope.classData[0].class_id;
+            $scope.populateSections($scope.classId)
+            
+        })
+        .error(function(data,success){
+        })
+
+         $scope.populateSections = function(classId){
+             $scope.secId =[]; 
+            globalServices.getSections(classId)
+            .success(function(data, status){
+                $scope.secData = data.class_sections;// Api list-name
+                $scope.secId = $scope.secData[0].section_id;
+                $scope.populateSubjects($scope.secId);
+                
+            })
+            .error(function(data,success){
+                
+                $scope.populateSubjects($scope.secId);
+            })
+        } 
+
+          $scope.populateSubjects = function(secId){
+            $scope.subData = [];
+            subjectsServices.getSubjects(secId)
+            .success(function(data, status){
+                $scope.subData = data.subjects;
+                $scope.subId = $scope.subData[0].subject_id;
+                $scope.getChapters($scope.subId);
+            })
+            .error(function(data,success){
+            });
+        }
 
           $scope.showRole = function(role){            
             return globalServices.fetchRoleAuth(role);
