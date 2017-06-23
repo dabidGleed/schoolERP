2bdde276098035deeec6bc5d0dca963eac3295fd
chmahi <maheswari.chnaidu@gmail.com>
2017-06-23 17:39:19 +0530

    added changes

--- app/components/studentAttendance/studentAttendanceController.js
+++ app/components/studentAttendance/studentAttendanceController.js
@@ -1,71 +1,70 @@
 angular.module('school_erp')
-.controller("studentAttendanceController",['$http','$scope','studentServices','globalServices','ngDialog', function($http, $scope, studentServices, globalServices, ngDialog){
+    .controller("studentAttendanceController", ['$http', '$scope', 'studentServices', 'globalServices', 'ngDialog', function($http, $scope, studentServices, globalServices, ngDialog) {
         $scope.studentData = [];
         $scope.classDatanew = [];
+        $scope.all = [];
+
 
         globalServices.getClass()
-        .success(function(data, status){
-            $scope.classDatanew = data.school_classes;// Api list-name
-            $scope.classId = $scope.classDatanew[0].class_id;
-            $scope.populateSections($scope.classId);            
-        })
-        .error(function(data,success){
-        })
+            .success(function(data, status) {
+                $scope.classDatanew = data.school_classes; // Api list-name
+                $scope.classId = $scope.classDatanew[0].class_id;
+                $scope.populateSections($scope.classId);
+            })
+            .error(function(data, success) {})
 
-         $scope.populateSections = function(classId){
+        $scope.populateSections = function(classId) {
             globalServices.getSections(classId)
-            .success(function(data, status){
-                $scope.secData = data.class_sections;// Api list-name
-                $scope.secId = $scope.secData[0].section_id;
-                $scope.getStudents($scope.secId);
-            })
-            .error(function(data,success){
-            })
+                .success(function(data, status) {
+                    $scope.secData = data.class_sections; // Api list-name
+                    $scope.secId = $scope.secData[0].section_id;
+                    $scope.getStudents($scope.secId);
+                })
+                .error(function(data, success) {})
         }
 
-        $scope.getStudents = function(classSecValue){
+        $scope.getStudents = function(classSecValue) {
 
             studentServices.getStudents(classSecValue)
-            .success(function(data, status){
-                $scope.studentData = data.students;
-            })
-            .error(function(data,success){
-            })
+                .success(function(data, status) {
+                    $scope.studentData = data.students;
+                })
+                .error(function(data, success) {})
         }
-        $scope.addAttendance = function(studentVal, status){
-             var Attendance = {
+        $scope.addAttendance = function(studentVal, status) {
+            var Attendance = {
                 class_id: $scope.classId,
                 section_id: $scope.secId,
                 session: "morning",
-                status: status    
-             }
-           console.log(Attendance);
+                status: status
+            }
+            console.log(Attendance);
 
             studentServices.setAttendance(Attendance, studentVal.student_id)
-            .success(function(data, status){
-                
-                ngDialog.open({
-                template: '<p> Student Attendance  submitted successfully </p>',
-                plain: true
-                });
-                $scope.data = [];
-                
-            })
-            .error(function(data,success){
-                 ngDialog.open({
-                template: '<p>Some Error Occured!</p>',
-                plain: true
-                });
-            })
+                .success(function(data, status) {
+
+                    ngDialog.open({
+                        template: '<p> Student Attendance  submitted successfully </p>',
+                        plain: true
+                    });
+                    $scope.data = [];
+
+                })
+                .error(function(data, success) {
+                    ngDialog.open({
+                        template: '<p>Some Error Occured!</p>',
+                        plain: true
+                    });
+                })
         }
 
 
         $scope.students = [];
-       $scope.attn = function(student,status){
+        $scope.attn = function(student, status) {
             var noMatch = true;
             var obj = {
-                student_id:student.student_id,
-                status:status
+                student_id: student.student_id,
+                status: status
             }
 
             // $scope.students.forEach(function(students) {             
@@ -81,15 +80,25 @@ angular.module('school_erp')
             //     $scope.students.push(obj);
             //     noMatch = false;
             // }
-            
+
             var students;
-            while(students.student_id == student.student_id){
-                 students.status = status;
+            while (students.student_id == student.student_id) {
+                students.status = status;
             }
             //  console.log(obj);
         }
-       
-       
 
-}])
+        $scope.attendanceStatus = function() {
+            $scope.studentData.forEach(function(element) {
+                var obj = {
+                    student_id: element.student_id,
+                    status: "None"
+                }
+                $scope.all.push(obj);
 
+            });
+
+
+            console.log($scope.all);
+        }
+    }])
--- app/components/subjects/subjectsController.js
+++ app/components/subjects/subjectsController.js
@@ -1,73 +1,71 @@
 angular.module('school_erp')
-.controller("subjectsController",['$http','$scope','subjectsServices','ngDialog','globalServices', function($http, $scope, subjectsServices, ngDialog, globalServices){
-        $scope.subjectsData = [];    
+    .controller("subjectsController", ['$http', '$scope', 'subjectsServices', 'ngDialog', 'globalServices', function($http, $scope, subjectsServices, ngDialog, globalServices) {
+        $scope.subjectsData = [];
         $scope.data = [];
         $scope.classData = [];
         $scope.secId = '';
         $scope.secData = [];
         $scope.classId = '';
         globalServices.getClass()
-        .success(function(data, status){
-            $scope.classData = data.school_classes;// Api list-name
-            $scope.classId = $scope.classData[0].class_id;
-            $scope.populateSections($scope.classId)
-            
-        })
-        .error(function(data,success){
-        })
+            .success(function(data, status) {
+                $scope.classData = data.school_classes; // Api list-name
+                $scope.classId = $scope.classData[0].class_id;
+                $scope.populateSections($scope.classId)
 
-         $scope.populateSections = function(classId){
-             $scope.secId = '';
+            })
+            .error(function(data, success) {})
+
+        $scope.populateSections = function(classId) {
             globalServices.getSections(classId)
-            .success(function(data, status){
-                $scope.secData = data.class_sections;// Api list-name
-                $scope.secId = $scope.secData[0].section_id;
-                $scope.getSubjects($scope.secId);
-            })
-            .error(function(data,success){
-            })          
-                   
+                .success(function(data, status) {
+                    console.log($scope.secId);
+                    $scope.secData = data.class_sections; // Api list-name
+                    // $scope.secId = $scope.secData[0].section_id;
+                    $scope.getSubjects($scope.secId);
+                })
+                .error(function(data, success) {})
+
         }
 
 
-        $scope.getSubjects = function(secId){
+        $scope.getSubjects = function(secId) {
+
             subjectsServices.getSubjects(secId)
-            .success(function(data, status){
-                $scope.subjectsData = data.subjects;
-            })
-            .error(function(data,success){
-            });
+                .success(function(data, status) {
+                    $scope.subjectsData = data.subjects;
+                })
+                .error(function(data, success) {});
         }
 
-         $scope.addSubjects = function(data,sec){
-             var Subjects = {
-               _id:"",
-               subject_id: $scope.data.subject_id,
-               class_id: "",
-               name:$scope.data.name 
-             }
-            subjectsServices.setSubjects(Subjects, sec)   
-            .success(function(data, status){
-                ngDialog.open({
-                template: '<p>Subjects are Added Successfully.</p>',
-                plain: true
-                });
-                $scope.data = [];
-                $scope.getSubjects($scope.secId);
-            })
-            .error(function(data,success){
-                ngDialog.open({
-                template: '<p>Some Error Occured!</p>',
-                plain: true
-                });
-            })
-           
+        $scope.addSubjects = function(data) {
+            console.log($scope.secId);
+            var Subjects = {
+                    _id: "",
+                    subject_id: $scope.data.subject_id,
+                    class_id: "",
+                    name: $scope.data.name
+                }
+                // subjectsServices.setSubjects(Subjects, sec)
+                //     .success(function(data, status) {
+                //         ngDialog.open({
+                //             template: '<p>Subjects are Added Successfully.</p>',
+                //             plain: true
+                //         });
+                //         $scope.data = [];
+                //         $scope.getSubjects($scope.secId);
+                //     })
+                //     .error(function(data, success) {
+                //         ngDialog.open({
+                //             template: '<p>Some Error Occured!</p>',
+                //             plain: true
+                //         });
+                //     })
+
         };
 
-       // $scope.getSubjects();
+        // $scope.getSubjects();
 
-         $scope.showRole = function(role){            
+        $scope.showRole = function(role) {
             return globalServices.fetchRoleAuth(role);
         }
-}])
-
+    }])
